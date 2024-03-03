5790dc56b1aaa5579481f22b2210f806ec111bcb
Joan14fran <99340081+Joan14fran@users.noreply.github.com>
2024-03-02 23:03:28 -0500

    Add models

---
+++ apps/qr_manager/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Agrega aquí las URL específicas para la aplicación qr_manager
+]
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ID_Usuario = models.AutoField(primary_key=True)
+    Nombre = models.CharField(max_length=30)
+    Apellido = models.CharField(max_length=30)
+
+    # Agrega related_name a los campos groups y user_permissions
+    groups = models.ManyToManyField('auth.Group', related_name='custom_user_groups', blank=True)
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_permissions', blank=True)
+
+    def __str__(self):
+        return self.username
