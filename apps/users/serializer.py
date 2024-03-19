512740b3931839045710f098706527f509f8e09b
Joan14fran <99340081+Joan14fran@users.noreply.github.com>
2024-03-19 18:17:31 -0500

    serializer user changes

--- apps/users/serializer.py
+++ apps/users/serializer.py
@@ -3,7 +3,7 @@ from django.contrib.auth.hashers import make_password
 from .models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=False)  # Hacer la contraseña opcional en actualizaciones
 
     class Meta:
         model = CustomUser
@@ -12,3 +12,11 @@ class CustomUserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data.get('password'))
         return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        # Eliminar el campo de contraseña si no se proporciona uno nuevo
+        if 'password' in validated_data:
+            validated_data['password'] = make_password(validated_data.get('password'))
+        else:
+            validated_data.pop('password', None)
+        return super().update(instance, validated_data)
